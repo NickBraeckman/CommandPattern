855567b485a2ca1def516b6e56f48b5ab9a86368
nick braeckman <nick.braeckman@gmail.com>
2020-03-14 08:04:44 +0100

    Initial commit

---
+++ src/AutoMotorCommand.java
@@ -0,0 +1,13 @@
+public class AutoMotorCommand implements Command {
+    private Garagist garagist;
+
+    public AutoMotorCommand(Garagist garagist){
+        this.garagist = garagist;
+    }
+
+    @Override
+    public void execute() {
+        garagist.vervangMotor();
+        garagist.checkBandenSpanning();
+    }
+}
---
+++ src/BrommerSpatbordCommand.java
@@ -0,0 +1,13 @@
+public class BrommerSpatbordCommand implements Command {
+    private Garagist garagist;
+
+    public BrommerSpatbordCommand(Garagist garagist){
+        this.garagist = garagist;
+    }
+
+    @Override
+    public void execute() {
+        garagist.vervangSpatBord();
+        garagist.checkBandenSpanning();
+    }
+}
